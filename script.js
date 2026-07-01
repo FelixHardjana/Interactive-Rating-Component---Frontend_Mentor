7c4994d424eeed83a3e2a3761bbbf84d58b86899
Felix <fhardjana@gmail.com>
2026-06-30 22:26:16 -0400

    completed the rating system

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 
 
 let buttonArray = null;
+let buttonPressed = null;
 
-const numbers = [];
+// const numbers = [];
+
+const rating_span = document.querySelector("#rating-review");
+const button_submit = document.getElementById("button-submit");
+
+
+const main_rating_panel = document.querySelector(".main-rating-panel");
+const rating_result = document.querySelector(".rating-result");
 
 for(let i = 0; i < 5; i++){
     buttonArray = document.createElement("button");
-    numbers[i] = i+1;
-    buttonArray.textContent = numbers[i];
-    document.querySelector(".rating-button").appendChild(buttonArray);
-    // buttonArray.style.backgroundColor = "blue";
+    // numbers[i] = i+1;
+    buttonArray.textContent = i+1;
 
+    buttonArray.addEventListener("click", () => {
+        buttonPressed = i+1;
+    });
+
+    document.querySelector(".rating-button").appendChild(buttonArray);
+        
 }
+
+button_submit.addEventListener("click", () => {
+    rating_span.textContent = buttonPressed; // this is to test out the rating
+    main_rating_panel.style.display = "none";
+    rating_result.style.display = "inline-flex"
+});
+
+
